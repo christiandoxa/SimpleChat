1d73eb8bbced1f4a38f7a155fe866d24ab18ff4b
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2016-10-01 07:52:02 +0700

    - Perubahan pada dependensi gradle - Penambahan empty activity baru dengan nama chatroom - Perubahan besar besaran pada Mainactivity - Penambahan 2 icon add dan send - Merubah layout main dan chatroom - Mengirim dan menerima pesan secara realtime bisa dilakukan

--- app/src/main/java/com/zhergiuz/simplechat/MainActivity.java
+++ app/src/main/java/com/zhergiuz/simplechat/MainActivity.java
@@ -1,37 +1,111 @@
 package com.zhergiuz.simplechat;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    TextView fireData; //Untuk menampilkan data dari firebase
-    Firebase ref; //Inisialisasi Firebase untuk project
+    private Button add_room;
+    private EditText room_name;
+    private ListView listView;
+    private String name;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Firebase.setAndroidContext(this);
 
-        fireData = (TextView) findViewById(R.id.TextViewFireBase);
-        ref = new Firebase("https://simple-chat-781cf.firebaseio.com/");
-        ref.addValueEventListener(new ValueEventListener() {
+        add_room = (Button) findViewById(R.id.btnAdd_room);
+        room_name = (EditText) findViewById(R.id.etName_room);
+        listView = (ListView) findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_of_rooms);
+        listView.setAdapter(arrayAdapter);
+
+        request_user_name();
+
+        add_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put(room_name.getText().toString(), "");
+                root.updateChildren(map);
+            }
+        });
+
+        root.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String superData = (String) dataSnapshot.getValue();
-                fireData.setText(superData);
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+                while (i.hasNext()) {
+                    set.add(((DataSnapshot) i.next()).getKey());
+                }
+                list_of_rooms.clear();
+                list_of_rooms.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(FirebaseError firebaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Intent I = new Intent(getApplicationContext(), chatroom.class);
+                I.putExtra("room_name", ((TextView) view).getText().toString());
+                I.putExtra("user_name", name);
+                startActivity(I);
+            }
+        });
+    }
+
+    private void request_user_name() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Masukkan Nama");
+        final EditText input_field = new EditText(this);
+        builder.setView(input_field);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                name = input_field.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+                request_user_name();
+            }
+        });
+        builder.show();
     }
 }
